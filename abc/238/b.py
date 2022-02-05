296af0b1fd9147255279f42123e890678c9aa6e3
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-02-05 21:25:26 +0900

    b

--- abc/238/b.py
+++ abc/238/b.py
@@ -2,11 +2,24 @@
 N = int(input())
 A= list(map(int, input().split()))
 
-start=0
-end=360
+#どれだけ回した位置を切ったか計算
+sum = 0
+for i, item in enumerate(A):
+  A[i] = sum + item
+  sum += item
+  #回した位置を360度に収める
+  A[i] = A[i]%360
 
-size = [start, end]
+# Aを小さ順で並び替えて、先頭と最後に0と360をたす
+A.sort()
+A.append(360)
+A.insert(0,0)
 
-size = A + size
-size.sort()
-print(size)
+#2点間の距離を取る
+max = 0
+for i in range(len(A)-1):
+  between = A[i+1]-A[i]
+  if (max < between):
+    max = between
+
+print(max)
